506f95ee00c0ce5c7839cd777d169cf861f20d4a
leonardo Gomes <leonardojgomes@hotmail.com>
2025-06-10 10:22:13 -0300

    tabuada

---
+++ tabuadaMult.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let a = prompt("digite um número: ")
+let mult 
+for ( let b = 0 ; b <=10 ; b++ ){
+    mult = a * b
+    console.log ("tabuada de "+ a +" é: " + a + " x " + b + " = "+ mult)
+}
+
+
+
+
+
+   
