8a69cd41ebf30330dc770176bbd00ff7effc54b8
leonardo Gomes <leonardojgomes@hotmail.com>
2025-06-10 09:30:39 -0300

    atividade realizada em sala

---
+++ BomDia.js
@@ -0,0 +1,5 @@
+//Aplicando bom dia
+
+for (let i = 1; i <=50; i++ ){
+    console.log("bom dia" ) ;
+}
---
+++ NumerosCem.js
@@ -0,0 +1,5 @@
+//Aplicando números
+
+for (let i = 1; i <=100; i++ ){
+    console.log( +i) ;
+}
